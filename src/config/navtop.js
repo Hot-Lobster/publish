37246aca7f07fab934b37b15ac48dcec55ffbe52
olsoa4 <olsoa4@hotmail.com>
2019-06-24 11:46:04 -0500

    navbar item fix

--- src/config/navtop.js
+++ src/config/navtop.js
@@ -4,7 +4,12 @@ import { Navbar, NavItem } from "react-materialize";
 export default class Nav extends React.Component {
   render() {
     return (
-      <Navbar brand="Adam Olson" className="" right>
+      <Navbar
+        brand="Adam Olson"
+        href={process.env.PUBLIC_URL + "/"}
+        className=""
+        right
+      >
         <NavItem href={process.env.PUBLIC_URL + "/projects"}>Projects</NavItem>
         <NavItem href={process.env.PUBLIC_URL + "/fish"}>Fish</NavItem>
         <NavItem href={process.env.PUBLIC_URL + "/"}>Home</NavItem>
